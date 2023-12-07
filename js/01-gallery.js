add10e13744bb0d81bcf58b216c1523ea321d611
WojD89 <wojciech.dyrka@gmail.com>
2023-12-07 21:39:45 +0100

    HW7 task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  const imgSrc = event.target;
+  console.log(imgSrc);
+  const lightbox = basicLightbox.create(
+    `<img src="${imgSrc.dataset.source}" >`
+  );
+  lightbox.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      lightbox.close();
+    }
+  });
+});
+const images = galleryItems.map((item) => {
+  return `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </div>`;
+});
+galleryContainer.insertAdjacentHTML("beforeend", images.join(""));
 
 console.log(galleryItems);
